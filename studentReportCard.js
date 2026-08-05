abefc231293b54f6728624d5990001f03919d58e
mahbub810515 <mahbub810515@gmail.com>
2026-08-05 11:12:42 +0600

    add validation and change the logic.

--- studentReportCard.js
+++ studentReportCard.js
@@ -1,30 +1,42 @@
 
-function generateReportCard(student){
-    let totalMarks=0;
-    let totalSubject=0;
-    let grade='';
-    
-    for(key in student){
-       let item =student[key];
-       if(typeof item !=='string'){
-            totalMarks+=item;
-            totalSubject++;
-       }
-    }
-    let average= totalMarks/totalSubject;
-    if(average>=90){
-        grade="A+";
-    }else if(average>=80){
-        grade="A";
-    
-    }else if(average>=70){
-        grade="B";
-    }else{
-        grade="F";
-    }
-    return {report:{name:student.name,totalMarks,average,grade}};
-}
+function generateReportCard(student) {
 
-let student = { name:'Ayan', bangla:78, english:85, math:92 };
+    if (typeof student !== "object" || Array.isArray(student) === true) {
+        return "Invalid for object";
+    }
+    if (typeof student.bangla !== "number" || typeof student.english !== "number" || typeof student.math !== "number") {
+        return "Invalid number";
+    }
+    //this code for getting dynamic  value
+    // for(key in student){
+    //    let item =student[key];
+    //    if(typeof item !=='string'){
+    //         totalMarks+=item;
+    //         totalSubject++;
+    //    }
+    // }
+    let totalMarks = student.bangla + student.english + student.math;
+    let grade = '';
+    let average = totalMarks / 3;
+    if (average >= 90) {
+        grade = "A+";
+    } else if (average >= 80) {
+        grade = "A";
+
+    } else if (average >= 70) {
+        grade = "B";
+    } else {
+        grade = "F";
+    }
+    return { report: { name: student.name, totalMarks, average, grade } };
+}
+//console the result  for testing the app
+let student = { name: 'Ayan', bangla: 78, english: 85, math: 92 };
 let result = generateReportCard(student);
 console.log(result);
+student = { name: 'Ayan', bangla: "78", english: 85, math: 92 };
+result = generateReportCard(student);
+console.log(result);
+student = [20, 58];
+result = generateReportCard(student);
+console.log(result)
