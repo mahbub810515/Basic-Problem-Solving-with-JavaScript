b17e01310ebfdfb928844fa857c5c98c05937566
mahbub810515 <mahbub810515@gmail.com>
2026-08-05 13:05:57 +0600

    add validatro for the app

--- cart-total.js
+++ cart-total.js
@@ -1,16 +1,28 @@
 function verifyCartTotal(itemPrices, displayedTotal){
+    if(!Array.isArray(itemPrices)){
+        return "Invalid";
+    }
+    if(typeof displayedTotal !=="number"){
+        return "Invalid";
+    }
     let totalPrice=0;
     for(itemPrice of itemPrices){{
-        totalPrice+=itemPrice;
-        console.log(itemPrice)
+        if(typeof itemPrice !=='number'){
+            return "Invalid";
+        }
+        totalPrice+=itemPrice;       
     }}
     if(totalPrice==displayedTotal){
         return true
-    }
-    console.log(totalPrice)
+    }return false;
 
 }
+
 let itemPrices=[100,50,40];
 let displayedTotal=190;
 let result = verifyCartTotal(itemPrices, displayedTotal);
-console.log(result)
+console.log(result);
+ itemPrices=[250,400,150];
+ displayedTotal=805;
+ result = verifyCartTotal(itemPrices, displayedTotal);
+console.log(result);
